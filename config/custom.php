facce806cd80c4f3b64ede01feef5aff6811ab41
James <James@gmail.com>
2019-07-08 11:44:30 +0800

    更换websocket服务器

--- config/custom.php
+++ config/custom.php
@@ -1,7 +1,7 @@
 <?php
 return [
     //推送消息请求地址
-    'PUSH_MESSAGE_URL' => 'http://demos.workerman.net:2121',
+    'PUSH_MESSAGE_URL' => 'http://103.100.62.69:2121',
     //推送消息登录地址
-    'PUSH_MESSAGE_LOGIN' => 'http://demos.workerman.net:2120',
+    'PUSH_MESSAGE_LOGIN' => 'http://103.100.62.69:2120',
 ];
